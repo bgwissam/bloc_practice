aedb741680755bef3032ebe711f5094887123d71
bgwissam <bgwissam@hotmail.com>
2022-01-10 06:37:58 +0300

    added counter bloc state

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,103 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/counter_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_cubit_bloc_tutorial/bloc/weather_bloc.dart';
-import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
-import 'package:flutter_cubit_bloc_tutorial/data/weather_repository.dart';
-import 'package:flutter_cubit_bloc_tutorial/pages/weather_search_page.dart';
 
-void main() => runApp(MyApp());
+void main(List<String> args) async {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Bloc Counter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.orange,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       home: BlocProvider(
-        create: (context) => WeatherBloc(FakeWeatherRepository()),
-        child: WeatherSearchPage(),
+        create: (context) => CounterCubit(),
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You have pushed the button to many fucking times'),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Increment'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Decrement'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'Oh, negative number no - ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                    softWrap: true,
+                  );
+                } else if (state.counterValue % 10 == 0) {
+                  return Text('Oh...at tenth number: ${state.counterValue}',
+                      style: Theme.of(context).textTheme.headline4);
+                } else if (state.counterValue % 2 == 0) {
+                  return Text('Yey, even number - ${state.counterValue}',
+                      style: Theme.of(context).textTheme.headline4);
+                }
+                return Text(state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4);
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
